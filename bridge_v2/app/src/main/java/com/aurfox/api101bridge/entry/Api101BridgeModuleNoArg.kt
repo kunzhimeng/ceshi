37acc1e08908607d5c1b74d499f4457b1fe51552
kunzhimeng <75667603+kunzhimeng@users.noreply.github.com>
2026-03-23 00:03:32 +0800

    Implement logging in Api101BridgeModuleNoArg
    Added logging for module loading events.

--- bridge_v2/app/src/main/java/com/aurfox/api101bridge/entry/Api101BridgeModuleNoArg.kt
+++ bridge_v2/app/src/main/java/com/aurfox/api101bridge/entry/Api101BridgeModuleNoArg.kt
@@ -1,5 +1,6 @@
 package com.aurfox.api101bridge.entry
 
+import android.util.Log
 import com.aurfox.api101bridge.bridge.BridgeRuntime
 import io.github.libxposed.api.XposedModule
 import io.github.libxposed.api.XposedModuleInterface.ModuleLoadedParam
@@ -7,10 +8,12 @@ import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
 
 class Api101BridgeModuleNoArg : XposedModule() {
     override fun onModuleLoaded(param: ModuleLoadedParam) {
+        Log.e("API101Bridge", "onModuleLoaded called")
         BridgeRuntime.attachHost(this, param)
     }
 
     override fun onPackageLoaded(param: PackageLoadedParam) {
+        Log.e("API101Bridge", "onPackageLoaded called")
         BridgeRuntime.dispatchPackageLoaded(param)
     }
 }
