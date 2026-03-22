cf894316cc7db6d856c55854bc9b3e9665386822
kunzhimeng <75667603+kunzhimeng@users.noreply.github.com>
2026-03-22 22:52:09 +0800

    Update Api101BridgeModule.kt

--- bridge_v2/app/src/main/java/com/aurfox/api101bridge/Api101BridgeModule.kt
+++ bridge_v2/app/src/main/java/com/aurfox/api101bridge/Api101BridgeModule.kt
@@ -1,10 +1,16 @@
-package com.aurfox.api101bridge
+package com.aurfox.api101bridge.entry
 
-/**
- * Deprecated placeholder kept only so earlier references do not break.
- *
- * Use one of the concrete entry classes under `com.aurfox.api101bridge.entry`:
- * - Api101BridgeModuleNoArg
- * - Api101BridgeModuleCtor
- */
-object Api101BridgeModule
+import com.aurfox.api101bridge.bridge.BridgeRuntime
+import io.github.libxposed.api.XposedModule
+import io.github.libxposed.api.XposedModuleInterface.ModuleLoadedParam
+import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
+
+class Api101BridgeModuleNoArg : XposedModule() {
+    override fun onModuleLoaded(param: ModuleLoadedParam) {
+        BridgeRuntime.attachHost(this, param)
+    }
+
+    override fun onPackageLoaded(param: PackageLoadedParam) {
+        BridgeRuntime.dispatchPackageLoaded(param)
+    }
+}
