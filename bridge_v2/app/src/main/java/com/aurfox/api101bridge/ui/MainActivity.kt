3710d3dbecc9ba0171f722d7307065e69ec856de
kunzhimeng <75667603+kunzhimeng@users.noreply.github.com>
2026-03-23 20:33:01 +0800

    Update MainActivity.kt

--- bridge_v2/app/src/main/java/com/aurfox/api101bridge/ui/MainActivity.kt
+++ bridge_v2/app/src/main/java/com/aurfox/api101bridge/ui/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(TextView(this).apply {
-            text = "API101 Bridge Host PROBE-0323-KF-MERGED-777"
+            text = "API101 Bridge Host PROBE-0323-KEF-PROBE-888"
             textSize = 18f
             setPadding(48, 48, 48, 48)
         })
