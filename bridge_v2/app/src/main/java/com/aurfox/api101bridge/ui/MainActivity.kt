bfb1d581dcbe5ddf5a0336ec7937bbe7e39b7df7
kunzhimeng <75667603+kunzhimeng@users.noreply.github.com>
2026-03-23 17:15:15 +0800

    Update MainActivity.kt

--- bridge_v2/app/src/main/java/com/aurfox/api101bridge/ui/MainActivity.kt
+++ bridge_v2/app/src/main/java/com/aurfox/api101bridge/ui/MainActivity.kt
@@ -8,7 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(TextView(this).apply {
-            text = "API101 Bridge Host PROBE-0323-I-HOST-CLASSLOADER-444"
+            text = "API101 Bridge Host PROBE-0323-I-HOST-CLASSLOADER-444 FINAL"
+            textSize = 18f
+            setPadding(48, 48, 48, 48)
         })
     }
 }
